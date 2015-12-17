1719ffbc77815a0fb471b46ef835e9564fc36471
Antonios <antwnis@gmail.com>
2015-12-17 17:42:04 +0000

    Single endpoint with encoded response

--- src/main/scala/com.landoop.avro4sui/App.scala
+++ src/main/scala/com.landoop.avro4sui/App.scala
@@ -16,56 +16,59 @@ trait Avro4sEndpoint {
     val handler = new ServletContextHandler
     handler.setContextPath("/")
 
-    // Avro => Scala Case Class
-    val avro2scala = new ServletHolder(new Avro2CaseClassServlet )
-    avro2scala.setAsyncSupported(true)
-    handler.addServlet(avro2scala, "/avro2scala")
-
-    // Json => Avro
-    val json2avro = new ServletHolder(new Json2AvroServlet )
-    json2avro.setAsyncSupported(true)
-    handler.addServlet(json2avro, "/json2avro")
+    // We try all possible transformations before failing
+    val servletAvro4s = new ServletHolder(new Avro4sServlet )
+    servletAvro4s.setAsyncSupported(true)
+    handler.addServlet(servletAvro4s, "/avro4s")
 
     val server = new Server(port)
     server.setHandler(handler)
     server.start()
 
-    println(s"Avro2Scala converter listening on port: $port")
+    println(s"Servlet for 'avro4s' convertions listening on port: $port")
   }
 }
 
-class Avro2CaseClassServlet extends HttpServlet {
+class Avro4sServlet extends HttpServlet {
   override def service(req: HttpServletRequest, resp: HttpServletResponse): Unit = {
+    println("Servicing")
+    if (req.getMethod.equalsIgnoreCase("OPTIONS")) {
+      println("OPTIONS")
+      resp.setContentType("text/plain")
+      resp.setHeader("Access-Control-Allow-Origin", "*")
+      resp.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS")
+      resp.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Key")
+      resp.getWriter.write("")
+    } else
     if (req.getMethod.equalsIgnoreCase("POST")) {
-      val postRequestObject = new Scanner(req.getInputStream, "UTF-8").useDelimiter("\\A").nextLine
+      println("POST")
+      val postRequestObject = new Scanner(req.getInputStream, "UTF-8").nextLine
+      println(postRequestObject )
       println("Detected POST with payload:" + postRequestObject)
+      var response = ""
       try {
         val modules = ModuleGenerator(postRequestObject)
         val templates = TemplateGenerator(modules)
         templates.foreach(t => println(t.definition + " ---- " + t.file))
-        resp.setContentType("text/html; charset=utf-8")
-        resp.getWriter.write(templates.head.definition)
+        response+="scala###" + templates.head.definition + "###"
+        println("[Avro] detected !!");
       } catch {
-        case (e:Throwable) => println(e)
+        case (e:Throwable) => println("Avro [NOT] Detected"); println(e)
       }
-    }
-  }
-}
-
-class Json2AvroServlet extends HttpServlet {
-  override def service(req: HttpServletRequest, resp: HttpServletResponse): Unit = {
-    if (req.getMethod.equalsIgnoreCase("POST")) {
-      val postRequestObject = new Scanner(req.getInputStream, "UTF-8").useDelimiter("\\A").nextLine
-      println("Detected [json=>avro] POST with payload:" + postRequestObject)
       try {
         val schema = new JsonToAvroConverter("com.test.avro").convert("MyClass", postRequestObject)
         val schemaString = schema.toString(true)
         println(schemaString)
-        resp.setContentType("text/html; charset=utf-8")
-        resp.getWriter.write(schemaString)
-      } catch {
-        case (e:Throwable) => println(e)
+        response+="avro###" + schemaString + "###"
+        println("[JSon] detected !!");
+     } catch {
+        case (e:Throwable) => println("JSon [NOT] Detected"); println(e)
       }
+      resp.setContentType("text/plain")
+      resp.setHeader("Access-Control-Allow-Origin", "*")
+      resp.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS")
+      resp.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Key")
+      resp.getWriter.write(response)
     }
   }
 }
@@ -73,23 +76,10 @@ class Json2AvroServlet extends HttpServlet {
 object App extends Avro4sEndpoint with App {
 
   println("Test Avro => Scala with:")
-  println("""  curl -X POST --data '{ "type" : "record", "name" : "MyClass", "namespace" : "com.test.avro", "fields" : [ { "name" : "foo", "type" : { "type" : "array", "items" : "boolean" } } ] }' http://localhost:1082/avro2scala""")
+  println("""  curl -X POST --data '{ "type" : "record", "name" : "MyClass", "namespace" : "com.test.avro", "fields" : [ { "name" : "foo", "type" : { "type" : "array", "items" : "boolean" } } ] }' http://localhost:1082/avro4s""")
   println("Test Json => Avro with:")
-
-  val exampleJson = """
-  {"menu": {
-    "id": "file",
-    "value": "File",
-    "popup": {
-    "menuitem": [
-  {"value": "New", "onclick": "CreateNewDoc()"},
-  {"value": "Open", "onclick": "OpenDoc()"},
-  {"value": "Close", "onclick": "CloseDoc()"}
-    ]
-  }
-  }}""".filter(_ >= ' ')
-
-  println(s"""  curl -X POST --data '$exampleJson' http://localhost:1082/json2avro""")
+  val exampleJson = """ {"menu": { "id": "file", "value": "File", "popup": { "menuitem": [ {"value": "New" }, {"value": "Open" }, {"value": "Close"} ] } }} """.filter(_ >= ' ')
+  println(s"""  curl -X POST --data '$exampleJson' http://localhost:1082/avro4s""")
 
   server(1082)
 }
